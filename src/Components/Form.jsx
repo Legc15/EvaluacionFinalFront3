87b6556b1437e663e1dd99bdc2ee09054f571ede
mmir22 <marcelomiraballes@gmail.com>
2023-09-09 13:45:17 -0300

    Se incorpora form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,19 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Form = () => {
     //Aqui deberan implementar el form completo con sus validaciones
 
+    const [formData, setFormData] = useState({
+        nombre: '',
+        email: '',
+    });
+
+    const [mensaje, setMensaje] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const validar = () => {
+            if (formData.nombre.length < 5 || !formData.email.includes('@')) {
+                return '<h3>Por favor verifique su información nuevamente<h3/>';
+            }
+
+            return `Gracias ${formData.nombre}, te contactaremos cuanto antes vía email.`;
+        };
+
+        const mensajeValidacion = validar();
+        setMensaje(mensajeValidacion);
+    };
+
     return (
         <div>
-            <form>
-                
-
-
-
-
+            <h2>Formulario de Registro</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="nombre">Nombre:</label>
+                    <input
+                        type="text"
+                        id="nombre"
+                        name="nombre"
+                        value={formData.nombre}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="email">Correo Electrónico:</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                    />
+                </div>
+                {mensaje && <p className={mensaje.includes('Gracias') ? 'exito' : 'error'}>{mensaje}</p>}
+                <button type="submit">Enviar</button>
             </form>
         </div>
     );
-};
+}
+
 
 export default Form;
