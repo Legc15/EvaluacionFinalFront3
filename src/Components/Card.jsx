a63ac060f462875c2f89d1e085bdfb078bd84419
mmir22 <marcelomiraballes@gmail.com>
2023-09-09 17:02:24 -0300

    Se renderizan las cards de cada odontologo en Home

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,11 +5,28 @@ const Card = ({ name, username, id }) => {
 
     const addFav = () => {
         // Aqui iria la logica para agregar la Card en el localStorage
+
+        const tarjeta = {
+            id,
+            name,
+            username
+        };
+
+        // Convertir la tarjeta a una cadena JSON
+        const tarjetaJSON = JSON.stringify(tarjeta);
+
+        // Almacenar la tarjeta en el localStorage con una clave única
+        localStorage.setItem(`tarjeta-${id}`, tarjetaJSON);
+
     }
 
     return (
         <div className="card">
             {/* En cada card deberan mostrar en name - username y el id */}
+
+            <h3>{name}</h3>
+            <p>Username: {username}</p>
+            <p>ID: {id}</p>
 
             {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,52 @@
-
-import "../index.css";
-import axios from "axios";
-import Card from "../Components/Card";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import Card from '../Components/Card';
 
 function Home() {
+    const [odontologos, setOdontologos] = useState([]);
 
+    useEffect(() => {
+        // Función para obtener datos de odontólogos de la API
+        const obtenerInfo = () => {
+            axios.get('https://jsonplaceholder.typicode.com/users')
+                .then((response) => {
+                    const datos = response.data;
+                    localStorage.setItem('datosAPI', JSON.stringify(datos));
+                    setOdontologos(datos);
+                })
+                .catch((error) => {
+                    console.error('Error al obtener datos de la API:', error);
+                });
+        };
 
-    //Se obtienen datos de odontologos de la api
-    const obtenerInfo = () => {
-
-        axios.get('https://jsonplaceholder.typicode.com/users')
-            .then(response => {
-                const datos = response.data;
-
-                localStorage.setItem('datosAPI', JSON.stringify(datos));
-            })
-            .catch(error => {
-                console.error('Error al obtener datos de la API:',error);
-            });
-
-    }
-
+        // Recuperar los datos del localStorage y convertirlos de nuevo a un objeto
+        const datosGuardados = localStorage.getItem('datosAPI');
+        if (datosGuardados) {
+            const datos = JSON.parse(datosGuardados);
+            setOdontologos(datos); // Establecer los odontólogos en el estado
+        } else {
+            // Si no hay datos en el localStorage, obtén los datos de la API y almacénalos
+            obtenerInfo();
+        }
+    }, []);
 
     return (
         <main>
             <h1>Nuestros Odontólogos</h1>
-            <div>
-
+            <div className="odontologos-list">
+                {odontologos.map((odontologo) => (
+                    <Link
+                        key={odontologo.id}
+                        to={`/home/${odontologo.id}`}
+                        className="odontologo-link"
+                    >
+                        <Card key={odontologo.id} name={odontologo.name} username={odontologo.username} id={odontologo.id} />
+                    </Link>
+                ))}
             </div>
         </main>
-    )
+    );
 }
 
-export default Home
+export default Home;
