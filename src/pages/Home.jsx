82cfebc3f8db6c4c8146469a570918417576e837
mmir22 <marcelomiraballes@gmail.com>
2023-09-09 12:04:28 -0300

    instala axios y modificamos home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,29 @@
 
 import "../index.css";
+import axios from "axios";
+import Card from "../Components/Card";
 
 function Home() {
+
+    const obtenerInfo = () => {
+
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
+    }
+
+
     return (
         <main>
             <h1>Nuestros Odontólogos</h1>
+            <div>
+
+            </div>
         </main>
     )
 }
