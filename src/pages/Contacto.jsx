9aba1a9d128453e9b0f11dab3f5c6b1ff9b81928
mmir22 <marcelomiraballes@gmail.com>
2023-09-14 16:30:54 -0300

    Se modifica diseno

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -6,7 +6,7 @@ function Contacto(){
         <div className='contacto'>
             <h1>Contact</h1>
             <h2>Want to know more?</h2>
-            <p>Send us your questions and we will contact you</p>
+            <p>Send us your information and we will contact you</p>
             <Form/>
         </div>
     )
